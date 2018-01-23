e9103270c0ce901b094ef234fd81285b2847926b
YvonneYYu <yeyang.yu@uqconnect.edu.au>
2018-01-23 15:48:27 +1000

    first entropy experiment succeeded

--- artificialSignalSandbox.py
+++ artificialSignalSandbox.py
@@ -1,7 +1,7 @@
 import numpy
 import matplotlib.pyplot as plt
 import calpy
-from calpy.testing.experiments import random_symbols
+from calpy.testing.experiments import random_symbols, symbolise
 from calpy.entropy.entropy import entropy_profile
 #from sklearn import cluster
 
@@ -9,51 +9,68 @@ from calpy.entropy.entropy import entropy_profile
 '''
 Pipeline 0
 '''
+fs, time_step = 8000, 0.025
 lower, upper, N = 75, 255, 3
+
+"""
 pitches = list(range(lower, upper, (upper - lower) // N))
 sin_waves = list()
-fs, time_step = 8000, 0.025
-
 t = numpy.arange(int(fs * time_step))
 for f in pitches:
     y = numpy.sin(2 * numpy.pi * f * t / fs)
     sin_waves.append(list(y))
 
-
-
-sig_a = []
-sig_b = []
-sig_c = []
-sig_d = []
+sig_a = numpy.array([])
+sig_b = numpy.array([])
+sig_c = numpy.array([])
 for i in range(N):
-    sig_a += sin_waves[i] #rising tone
-    sig_b += sin_waves[N - 1 - i] #falling tone
-    sig_c += sin_waves[N // 2] #monotonicity
+    sig_a = numpy.append(sig_a, sin_waves[i]) #rising tone
+    sig_b = numpy.append(sig_b, sin_waves[N - 1 - i]) #falling tone
+    sig_c = numpy.append(sig_c, sin_waves[N // 2]) #monotonicity
 
-sig_d = [0] * len(sig_a) #silence/pause
+sig_d = numpy.array([0] * len(sig_a)) #silence/pause
 
 #generate signal according to a distribution symbols [0, 1, 2, 3] rising, falling, level, silent
 p1 = [0.2, 0.2, 0.5, 0.1] # assumed as norm
 p2 = [0.4, 0.15, 0.375, 0.075] # increase rising
-p2 = [0.15, 0.4, 0.375, 0.075] # increase falling
-p3 = [0.18, 0.18, 0.44, 0.2] # increase pause
-l_norm = 6000 # length of symbols list of normal speech
-l_abnorm = 2400 # length of symbols list of abnormal speech
+p3 = [0.15, 0.4, 0.375, 0.075] # increase falling
+p4 = [0.13, 0.13, 0.34, 0.4] # increase pause
+l_norm = 4000 # length of symbols list of normal speech
+l_abnorm = 2000 # length of symbols list of abnormal speech
 ps = [p1, p2, p1, p3, p1, p4, p1]
 ls = [l_norm, l_abnorm, l_norm, l_abnorm, l_norm, l_abnorm, l_norm]
 
-symbols = []
+symbols = numpy.array([])
 for i in range(len(ps)):
-    symbols += random_symbols(ps[i], ls[i])
-print(type(symbols), len(symbols))
-keyboard
+    symbols = numpy.append(symbols, random_symbols(ps[i], ls[i]))
+symbols = symbols.astype(int)
+sig = numpy.array([])
+for symbol in symbols:
+    if symbol == 0:
+        sig = numpy.append(sig, sig_a)
+    elif symbol == 1:
+        sig = numpy.append(sig, sig_b)
+    elif symbol == 2:
+        sig = numpy.append(sig, sig_c)
+    else:
+        sig = numpy.append(sig, sig_d)
+numpy.save("signal", sig)
+numpy.save("symbols", symbols)
+"""
+symbols = numpy.load("symbols.npy")
+sig = numpy.load("signal.npy")
+
 #################################generate pitch#################################
 '''
 pipe line 1
 '''
 #set time_step == frame_size, no overlapping
+"""
 pitch_prof = calpy.dsp.pitch_profile(sig, fs, time_step = 0.025)
+numpy.save("pitch_prof", pitch_prof)
+"""
 
+pitch_prof = numpy.load("pitch_prof.npy")
 
 #mfcc_prof = calpy.dsp.mfcc_profile(sig, fs, time_step = 0.025)
 #mfcc_prof = mfcc_prof.T
@@ -76,22 +93,28 @@ feature_pitch = hist_N_median_profile(pitch_prof)
 db_pitch = cluster.DBSCAN(eps=0.1, min_samples=20).fit(feature_pitch)
 symbols = db_pitch.labels_
 '''
-symbols_hat = []
-for pitch in numpy.array_split(pitch_prof, len(pitches) // N):
-    symbols_hat.append( symbolise(pitch) )
 
+
+symbols_hat =numpy.array([])
+for pitch in numpy.array_split(pitch_prof, len(pitch_prof) // N):
+    symbols_hat = numpy.append( symbols_hat, symbolise(pitch) )
+
+numpy.save("symbols_hat", symbols_hat)
 ################################entropy#########################################
 '''
 pipe line 3
 '''
-entropy_durations = [300, 500, 700, 900, 1000, 1100, 1200]
+entropy_durations = [300, 500, 700, 900, 1000, 1100, 1200, 2000]
 for dur in entropy_durations:
     ent_prof = entropy_profile(symbols, window_size=dur)
     ent_prof_hat = entropy_profile(symbols_hat, window_size=dur)
 
     fig = plt.figure()
-    plt.plot(ent_prof, label="true entropy")
-    plt.plot(ent_prof_hat, label="model calculated entropy")
+    t = list(range(len(ent_prof)))
+    plt.plot(t, ent_prof, '+-', label="true entropy")
+    plt.plot(t, ent_prof_hat, '*-', label="model calculated entropy")
     plt.title("model calibration with entropy duration of {}s ({} symbols per entropy)".format(int(dur * N * 0.01), dur))
-    fig.legend()
+    plt.legend()
     fig.savefig("entropy_{}.png".format(dur))
+    numpy.save("entropy_{}".format(dur), ent_prof)
+    numpy.save("entropy_hat_{}".format(dur), ent_prof_hat)
