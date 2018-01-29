3aea08241c363418c6bc61176d2739aff7c023ef
YvonneYYu <yeyang.yu@uqconnect.edu.au>
2018-01-29 16:24:22 +1000

    restructure on master

--- __init__.py
+++
@@ -1,7 +0,0 @@
-from .testing import testing
-from .testing import experiments
-from .utilities import utilities
-from .dsp import dsp
-from .rqa import rqa
-from .plots import plots
-from .entropy import entropy
