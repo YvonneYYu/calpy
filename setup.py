6b53cef905d6c182a7f94e8cbedace1f47929945
YvonneYYu <yeyang.yu@uqconnect.edu.au>
2018-01-29 12:10:41 +1000

    add license to setup.py

--- setup.py
+++ setup.py
@@ -7,5 +7,6 @@ setup(name='calpy',
       author =['Yvonne Yu', 'Paul Vrbik'],
       author_email = ['yeyang.yu@uqconnect.edu.au', 'paulvrbik@gmail.com'],
       url = 'https://github.com/YvonneYYu/calpy',
+      license = 'MIT',
       keywords = ['Natural Language Understanding', 'Signal Processing']
 )
