e803a086947d4c5ae7a8610534a10f7095be0d13
YvonneYYu <yyy.austcer@gmail.com>
2018-01-29 21:59:42 +1000

    changed setup.py into version 0.1.1

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(name='calpy',
       packages = ['calpy'],
-      version='0.1',
+      version='0.1.1',
       description='Communication Analytics Lab Toolbox',
-      author =['Yvonne Yu', 'Paul Vrbik'],
+      author =['Yvonne Yu & Paul Vrbik'],
       author_email = ['yeyang.yu@uqconnect.edu.au', 'paulvrbik@gmail.com'],
       url = 'https://github.com/YvonneYYu/calpy',
       license = 'MIT',
